2e08827bcf761c1e18b9ea3e18357bb0e69b6301
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-06-03 17:16:47 -0700

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	hleft = binary_tree_height(tree->lef);
+	hleft = binary_tree_height(tree->left);
 	hright = binary_tree_height(tree->right);
 	if (hleft >= hright)
 		return (hleft + 1);
